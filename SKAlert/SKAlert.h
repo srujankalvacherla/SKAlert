411777fd38028587973068d9172564948a0c58e5
Srujan k <srujankalvacherla93@gmail.com>
2018-07-07 16:29:41 +0530

    Initial Commit

---
+++ SKAlert/SKAlert.h
@@ -0,0 +1,19 @@
+//
+//  SKAlert.h
+//  SKAlert
+//
+//  Created by Srujan k on 04/07/18.
+//  Copyright © 2018 Srujan k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKAlert.
+FOUNDATION_EXPORT double SKAlertVersionNumber;
+
+//! Project version string for SKAlert.
+FOUNDATION_EXPORT const unsigned char SKAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKAlert/PublicHeader.h>
+
+
---
+++ SKAlert/SKAlert.swift
@@ -0,0 +1,140 @@
+//
+//  SKAlert.swift
+//  SKAlert
+//
+//  Created by Srujan k on 04/07/18.
+//  Copyright © 2018 Srujan k. All rights reserved.
+//
+
+import UIKit
+
+public typealias OkCompletionHandler = (_ success:Bool) -> Void
+public typealias CancelCompletionHandler = (_ failure : Bool) -> Void
+
+
+public class SKAlert: UIViewController {
+    @IBOutlet private weak var headerTitle: UILabel!
+    @IBOutlet private weak var subTitle: UILabel!
+    @IBOutlet private weak var cancelButton: UIButton!
+    @IBOutlet private weak var okButton: UIButton!
+    @IBOutlet private weak var mainView: UIView!
+    @IBOutlet private weak var subView: UIView!
+    
+    var strongSelf: SKAlert?
+    let SKCornerRadius: CGFloat = 3.0
+    var contentView =  UIView()
+    let nibname = "SKAlert"
+    var OkayButtonTag = 1
+    let CancelButtonTag = 2
+    var okCompletion: OkCompletionHandler? = nil
+    var cancelCompletion: CancelCompletionHandler? =  nil
+
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+        strongSelf = self
+        self.setUp()
+    }
+    // MARK: - Set up
+    func setUp() {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: self.nibname, bundle: bundle)
+        self.view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        self.view.frame = UIScreen.main.bounds
+    }
+    func setUpContentView() {
+        strongSelf?.contentView.layer.cornerRadius = 5.0
+        strongSelf?.contentView.layer.borderWidth = 2
+        self.okButton.layer.cornerRadius = SKCornerRadius
+        self.cancelButton.layer.cornerRadius = SKCornerRadius
+        self.contentView.layer.masksToBounds = true
+
+    }
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public func showAlert(_ title: String, subTitle: String){
+        let window: UIWindow = UIApplication.shared.keyWindow!
+        window.addSubview(view)
+        window.bringSubview(toFront: view)
+        view.frame = window.bounds
+        self.setUpContentView()
+//        view.addSubview(self.subView)
+        strongSelf?.headerTitle.text = title
+        strongSelf?.subTitle.text = subTitle
+        strongSelf?.cancelButton.isHidden = true
+        strongSelf?.okButton.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        okCompletion = nil
+        cancelButton = nil
+        self.animateToSuperView()
+    }
+    public func showAlertWithOkAction(_ title: String, subTitle: String, ok: @escaping OkCompletionHandler){
+        let window: UIWindow = UIApplication.shared.keyWindow!
+        window.addSubview(view)
+        window.bringSubview(toFront: view)
+        view.frame = window.bounds
+        self.setUpContentView()
+        //        view.addSubview(self.subView)
+        strongSelf?.headerTitle.text = title
+        strongSelf?.subTitle.text = subTitle
+        strongSelf?.cancelButton.isHidden = true
+        strongSelf?.okButton.addTarget(self, action: #selector(buttonActon(_:)), for: .touchUpInside)
+        strongSelf?.okButton.tag = 1
+        self.okCompletion = ok
+        self.animateToSuperView()
+    }
+    public func showAlertWithTwoButtons(_ title: String, subTitle: String, ok: @escaping OkCompletionHandler, cancel: @escaping CancelCompletionHandler){
+        let window: UIWindow = UIApplication.shared.keyWindow!
+        window.addSubview(view)
+        window.bringSubview(toFront: view)
+        view.frame = window.bounds
+        self.setUpContentView()
+        view.addSubview(contentView)
+        strongSelf?.headerTitle.text = title
+        strongSelf?.subTitle.text = subTitle
+        strongSelf?.okButton.addTarget(self, action: #selector(buttonActon(_:)), for: .touchUpInside)
+        strongSelf?.okButton.tag = 1
+        strongSelf?.cancelButton.tag = 2
+        strongSelf?.cancelButton.addTarget(self, action: #selector(buttonActon(_:)), for: .touchUpInside)
+        self.okCompletion = ok
+        strongSelf?.cancelCompletion = cancel
+        self.animateToSuperView()
+    }
+    
+    // MARK: - Animate View
+    @objc func buttonActon(_ sender: UIButton) {
+        switch sender.tag {
+        case OkayButtonTag:
+            if self.okCompletion != nil{
+                self.okCompletion!(true)
+            }
+            break
+        case CancelButtonTag:
+            if self.cancelCompletion != nil{
+                self.cancelCompletion!(true)
+            }
+            break
+        default:
+            break
+        }
+        self.dismissView()
+        
+    }
+    @objc func dismissView() {
+        
+        UIView.animate(
+            withDuration: 0.15,
+            animations: {
+            self.subView.transform = CGAffineTransform(scaleX: 0.25, y: 0.5)
+        }) { _ in
+            self.view.removeFromSuperview()
+        }
+    }
+    fileprivate func animateToSuperView(){
+        self.subView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5 )
+        UIView.animate(withDuration: 0.25, animations: {
+            self.subView.transform = CGAffineTransform.identity
+            
+        })
+    }
+
+}
